b3c9c147c9370c328dae67b9b78ecc546ddf9b6d
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2024-02-03 18:05:24 +0300

    All 3 tasks of 3 homework are done!

--- third_homework/main.py
+++ third_homework/main.py
@@ -11,7 +11,8 @@ for i in list_1:
 print(count)
 '''
 
-list_1 = [1,8, 2, 3, 4, 5]
+# Поиск ближайшего по значению элемента в списке
+list_1 = [1, 8, 2, 3, 4, 5]
 k = 5
 min = abs(k - list_1[0])
 i = 0
@@ -22,3 +23,32 @@ for i in range(len(list_1)):
         min =  abs(k - list_1[i])
         index = i
 print(list_1[index])
+
+#Игра Скрабл. Подсчёт стоимомти введённого пользователем слова.
+'''
+dictionary = {}
+dictionary ={'up': '↑', 'left': '←', 'down': '↓', 'right': '→'}
+print(dictionary) # {'up':'↑', 'left':'←', 'down':'↓', 'right':'→'}
+print(dictionary['left']) # ←
+# типы ключей могут отличаться
+print(dictionary['up']) # ↑
+# типы ключей могут отличаться
+dictionary['left'] = '⇐'
+print(dictionary['left']) # ⇐
+print(dictionary['type']) # KeyError: 'type'
+del dictionary['left'] # удаление элемента
+for item in dictionary: # for (k,v) in dictionary.items():
+print('{}: {}'.format(item, dictionary[item]))
+# up: ↑
+# down: ↓
+# right: →
+'''
+k = 'hel'
+k = k.upper()
+cost = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
+res = 0
+for i in k:
+    for j in cost: # j принимает значения ключей
+        if i in cost[j]: # cost[j] это значение под j-ым ключом
+            res = res + j
+print(res)
